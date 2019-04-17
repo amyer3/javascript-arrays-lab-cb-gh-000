558c237a71652555376695520f608805238df20a
Alexander Myers <alexander.j.myers@icloud.com>
2019-04-17 00:13:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 var kittens = [] //define your array here
 
 // Add your functions and code here
+kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name){
+  
+}
+function destructivelyPrependKitten(name){
+  
+}
