f92f97a5d6aa5fb354ba8289ce7c4113fa428773
Alexander Myers <alexander.j.myers@icloud.com>
2019-04-17 00:31:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ function prependKitten(name){
 }
 
 function removeLastKitten(){
-  return kittens.splice(0, kittens.length -1)
+  return kittens.splice(0, kittens.length -2)
 }
 
 function removeFirstKitten(){
