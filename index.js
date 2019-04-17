d65ac9a2a240eeb769d4eff0cd2724c23e00e96c
Alexander Myers <alexander.j.myers@icloud.com>
2019-04-17 00:25:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,8 @@ function destructivelyRemoveFirstKitten(){
 }
 
 function appendKitten(name){
-  var array = kittens
+  var array = []
+  array = kittens
   array.push(name)
   console.log(array)
   return array
